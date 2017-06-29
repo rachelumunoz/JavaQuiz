abb96a6f98daf200f188f6cd349d82797ea52271
Rachel Munoz <rachelumunoz@gmail.com>
2017-06-29 15:28:39 -0700

    Adds array of questions

--- app/src/main/java/io/rachelmunoz/javaquiz/QuizActivity.java
+++ app/src/main/java/io/rachelmunoz/javaquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package io.rachelmunoz.javaquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -9,7 +10,15 @@ import org.w3c.dom.Text;
 public class QuizActivity extends AppCompatActivity {
 
     private TextView mQuestionTextView;
-    private Question mCurrentQuestion = new Question(R.string.question_one, true);
+
+    private Question[] mQuestionBank = new Question[] {
+        new Question(R.string.question_one, true),
+        new Question(R.string.question_two, false),
+        new Question(R.string.question_three, false),
+    };
+
+    private int mCurrentIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +26,10 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        int questionTextId = mCurrentQuestion.getTextResId();
+        int questionTextId = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(questionTextId);
+
+
 
     }
 }
