f398abe03585a1c3c8f2a23993ea9dd710aef79f
Rachel Munoz <rachelumunoz@gmail.com>
2017-06-29 15:14:22 -0700

    Add Question class, inflate TextView with model Question data

--- app/src/main/java/io/rachelmunoz/javaquiz/QuizActivity.java
+++ app/src/main/java/io/rachelmunoz/javaquiz/QuizActivity.java
@@ -2,12 +2,23 @@ package io.rachelmunoz.javaquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private TextView mQuestionTextView;
+    private Question mCurrentQuestion = new Question(R.string.question_one, true);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int questionTextId = mCurrentQuestion.getTextResId();
+        mQuestionTextView.setText(questionTextId);
+
     }
 }
